233b285bfd53584b4d1ab69ffe32daed291caeee
moeymoe111 <mohannadosman56@gmail.com>
2023-09-19 08:37:15 -0400

    Added KeyboardInputs class to handle key events

--- src/inputs/KeyboardInputs.java
+++ src/inputs/KeyboardInputs.java
@@ -1,4 +1,44 @@
 package inputs;
 
-public class KeyboardInputs {
+import main.GamePanel;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyboardInputs implements KeyListener {
+private GamePanel gamePanel;
+
+    public KeyboardInputs (GamePanel gamePanel){ // TO CHANGE THE VALUES OF DELTA
+        this.gamePanel =  gamePanel;
+
+    }
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+     }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+        switch(e.getKeyCode()) {
+
+            case KeyEvent.VK_W:
+                gamePanel.changeYDelta(-5);
+                break;
+            case KeyEvent.VK_S:
+                gamePanel.changeYDelta(5);
+                break;
+            case KeyEvent.VK_A:
+                gamePanel.changeXDelta(-5);
+                break;
+            case KeyEvent.VK_D:
+                gamePanel.changeXDelta(5);
+                break;
+
+        }
+    }
 }
